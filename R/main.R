f9c8f97e8aab60323c43bbe3aa653b927ef340a9
Woosub Shin <woosubs@umich.edu>
2020-10-01 19:04:26 +0200

    started initial commits

--- R/main.R
+++ R/main.R
@@ -1,2 +1,14 @@
 # main.R
-# Running and the functions
+# Running the functions
+
+library(dplyr)
+library(igraph)
+library(limma)
+library(tidyr)
+
+BASE_DIR <- "~/Desktop/BiGCAT/github/PathwayInteraction/"
+DATA_DIR <- file.path(BASE_DIR, "data")
+setwd(BASE_DIR)
+
+# load limma result
+limma_result <- read.csv(file=file.path(DATA_DIR, "limma_result_geomean.csv"))
